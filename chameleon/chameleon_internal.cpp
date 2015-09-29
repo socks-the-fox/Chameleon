3eeef4894b3b1289a7e2ead5287c40eebe44cfe1
Socks the Fox <jade@socksthefox.net>
2015-09-29 12:46:08 -0500

    Improve color code to be more correct, and utilize SMD more

--- chameleon/chameleon_internal.cpp
+++ chameleon/chameleon_internal.cpp
@@ -1,14 +1,13 @@
 #include <cstdint>
 #include <intrin.h>
+#include <math.h>
 #include "chameleon.h"
 #include "chameleon_internal.h"
 
 void fixRGB(ColorStat *color)
 {
-	__m128 countm;
-	countm.m128_f32[0] = countm.m128_f32[1] = countm.m128_f32[2] = color->count;
-	countm.m128_f32[3] = 1.0f;
-	color->rgbc = _mm_div_ps(color->rgbc, countm);
+	// The _mm_set_ps starts with the 1 (for keeping the count the same) because of the whole little-endian deal
+	color->rgbc = _mm_div_ps(color->rgbc, _mm_set_ps(1, color->count, color->count, color->count));
 }
 
 void calcYUV(ColorStat *color)
@@ -43,20 +42,29 @@ float saturation(const ColorStat *color)
 
 float distance(const ColorStat *c1, const ColorStat *c2)
 {
-	// TODO: SIMDize this
-	float x = c1->y - c2->y;
-	float y = c1->u - c2->u;
-	float z = c1->v - c2->v;
+	// Don't bother with sqrt-ing because we only care that A *is* further from B, but not by how much.
+	__m128 xyz = _mm_sub_ps(c1->yuve, c2->yuve);
+	xyz = _mm_mul_ps(xyz, _mm_set_ps(0, 1, 1, 1));
+	xyz = _mm_mul_ps(xyz, xyz);
 
-	return x*x + y*y + z*z;
+	// Horizontal add would be nice here...
+	return xyz.m128_f32[0] + xyz.m128_f32[1] + xyz.m128_f32[2] + xyz.m128_f32[3];
 }
 
 float contrast(const ColorStat *c1, const ColorStat *c2)
 {
-	// TODO: actual sRGB conversion stuff
 	// TODO: SIMDize this?
-	float L1 = 0.2126f * c1->r + 0.7152f * c1->g + 0.0722f * c1->b;
-	float L2 = 0.2126f * c2->r + 0.7152f * c2->g + 0.0722f * c2->b;
+	float r1 = (c1->r > 0.03928) ? pow((c1->r + 0.055f) / 1.055f, 2.4f) : c1->r / 12.92f;
+	float g1 = (c1->g > 0.03928) ? pow((c1->g + 0.055f) / 1.055f, 2.4f) : c1->g / 12.92f;
+	float b1 = (c1->b > 0.03928) ? pow((c1->b + 0.055f) / 1.055f, 2.4f) : c1->b / 12.92f;
+
+	float L1 = 0.2126f * r1 + 0.7152f * g1 + 0.0722f * b1;
+
+	float r2 = (c2->r > 0.03928) ? pow((c2->r + 0.055f) / 1.055f, 2.4f) : c2->r / 12.92f;
+	float g2 = (c2->g > 0.03928) ? pow((c2->g + 0.055f) / 1.055f, 2.4f) : c2->g / 12.92f;
+	float b2 = (c2->b > 0.03928) ? pow((c2->b + 0.055f) / 1.055f, 2.4f) : c2->b / 12.92f;
+
+	float L2 = 0.2126f * r2 + 0.7152f * g2 + 0.0722f * b2;
 
 	if (L1 > L2)
 	{
