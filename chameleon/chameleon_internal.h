caf369fb05d39aa29eb7a0f260059b3d8d9d5dbe
Socks the Fox <jade@socksthefox.net>
2015-11-09 17:28:39 -0600

    Reduce color space for Chameleon's color averaging and selection.

--- chameleon/chameleon_internal.h
+++ chameleon/chameleon_internal.h
@@ -49,11 +49,11 @@ struct Chameleon
 
 inline uint16_t XRGB5(uint32_t c)
 {
-	uint8_t b = (c >> 20) & 0x0F;
-	uint8_t g = (c >> 12) & 0x0F;
-	uint8_t r = (c >>  4) & 0x0F;
+	uint8_t b = (c >> 22) & 0x03;
+	uint8_t g = (c >> 14) & 0x03;
+	uint8_t r = (c >>  6) & 0x03;
 
-	return (r << 8) | (g << 4) | b;
+	return (r << 4) | (g << 2) | b;
 }
 
 void fixRGB(ColorStat *color);
